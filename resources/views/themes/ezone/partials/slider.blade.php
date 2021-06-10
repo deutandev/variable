fbc468dac311386b839fe6ff176cfdbdd3d09552
Andika Fathin Azfar <deutandev@pm.me>
2021-06-10 14:26:23 +0700

    Change slider size

--- resources/views/themes/ezone/partials/slider.blade.php
+++ resources/views/themes/ezone/partials/slider.blade.php
@@ -2,8 +2,11 @@
 	<div class="slider-area">
 		<div class="slider-active owl-carousel">
 			@foreach ($slides as $slide)
-			<div class="single-slider-4 slider-height-6 bg-img" style="background-image: url({{ asset('storage/'. $slide->extra_large) }})">
-				<div class="container">
+			<div class="single-slider-4">
+				<div class="carousel-item active">
+					<img src="{{ asset('storage/'. $slide->extra_large) }}" alt="" class="d-block w-100">
+				</div>
+				{{-- <div class="container">
 					<div class="row">
 						<div class="ml-auto col-lg-6">
 							<div class="furniture-content fadeinup-animated">
@@ -13,35 +16,9 @@
 							</div>
 						</div>
 					</div>
-				</div>
+				</div> --}}
 			</div>
 			@endforeach
-			{{-- <div class="single-slider-4 slider-height-6 bg-img" style="background-image: url({{ asset('themes/ezone/assets/img/slider/9.jpg') }})">
-				<div class="container">
-					<div class="row">
-						<div class="ml-auto col-lg-6">
-							<div class="furniture-content fadeinup-animated">
-								<h2 class="animated">Comfort  <br>Collection.</h2>
-								<p class="animated">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-								<a class="furniture-slider-btn btn-hover animated" href="product-details.html">Shop Now</a>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="single-slider-4 slider-height-6 bg-img" style="background-image: url({{ asset('themes/ezone/assets/img/slider/19.jpg') }})">
-				<div class="container">
-					<div class="row">
-						<div class="ml-auto col-lg-6">
-							<div class="furniture-content fadeinup-animated">
-								<h2 class="animated">Comfort  <br>Collection.</h2>
-								<p class="animated">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-								<a class="furniture-slider-btn btn-hover animated" href="product-details.html">Shop Now</a>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div> --}}
 		</div>
 	</div>
 @endif
